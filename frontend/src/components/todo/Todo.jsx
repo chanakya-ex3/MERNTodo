acfc98b68ab643d8b2f02f3b72ff8d2b11b84aa4
hp113 <hariprasad2315@gmail.com>
2024-03-13 07:11:39 +0530

    todo-cards created

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Todo.css";
+import TodoCards from "./TodoCards";
 const Todo = () => {
+  const [Inputs, setInputs] = useState({ title: "", body: "" });
+  const [Array, setArray] = useState([]);
   const show = () => {
     document.getElementById("textarea").style.display = "block";
   };
-  const change=()=>{}
+  const change = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...Inputs, [name]: value });
+  };
+  const submit = () => {
+    setArray([...Array, Inputs]);
+    setInputs({ title: "", body: "" });
+  };
   return (
     <div className="todo">
-      <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
-        <div className="d-flex flex-column todo-inputs-div w-50 p-1">
-          <input 
-          type="text" 
-          placeholder="TITLE" 
-          className="my-2 p-2 todo-inputs" 
-          onClick={show} 
-          name="title"
-          onChange={change}/>
-          <textarea 
-          id="textarea" 
-          type="text" 
-          placeholder="BODY" 
-          name="body"
-          className="p-2 todo-inputs" 
-          onChange={change}/>
-
-
+      <div className="todo-main container d-flex justify-content-center align-items-center flex-column">
+        <div className="d-flex flex-column todo-inputs-div w-50 p-1 ">
+          <input
+            type="text"
+            placeholder="TITLE"
+            className="my-2 p-2 todo-inputs"
+            name="title"
+            onClick={show}
+            value={Inputs.title}
+            onChange={change}
+          />
+          <textarea
+            id="textarea"
+            type="text"
+            placeholder="BODY"
+            className="p-2 todo-inputs"
+            name="body"
+            value={Inputs.body}
+            onChange={change}
+          />
         </div>
-        <div className='w-50 d-flex justify-content-end my-3'>
-          <button className='home-btn px-2 py-1'>Add</button>
+        <div className="w-50 d-flex justify-content-end my-3">
+          <button className="home-btn px-2 py-1" onClick={submit}>
+            Add
+          </button>
+        </div>
+      </div>
+      <div className="todo-body">
+        <div className="container-fluid">
+          <div className="row">
+            
+              {Array &&
+                Array.map((item, index) => (
+                  <div className="col-lg-3 col-10 mx-5 my-2">
+                    <TodoCards title={item.title} body={item.body}/>
+                    </div>
+                ))}
+            
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
